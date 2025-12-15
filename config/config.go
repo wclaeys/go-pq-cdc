a738ca4019d04811d6b35cbfb09ccd9d433d981c
wclaeys <2163252+wclaeys@users.noreply.github.com>
2025-12-15 20:54:38 +0000

    Remove unused ExtensionSupport

--- config/config.go
+++ config/config.go
@@ -15,19 +15,18 @@ import (
 )
 
 type Config struct {
-	Heartbeat        HeartbeatConfig    `json:"heartbeat" yaml:"heartbeat"`
-	Logger           LoggerConfig       `json:"logger" yaml:"logger"`
-	Host             string             `json:"host" yaml:"host"`
-	Username         string             `json:"username" yaml:"username"`
-	Password         string             `json:"password" yaml:"password"`
-	Database         string             `json:"database" yaml:"database"`
-	Publication      publication.Config `json:"publication" yaml:"publication"`
-	Slot             slot.Config        `json:"slot" yaml:"slot"`
-	Snapshot         SnapshotConfig     `json:"snapshot" yaml:"snapshot"`
-	Port             int                `json:"port" yaml:"port"`
-	Metric           MetricConfig       `json:"metric" yaml:"metric"`
-	DebugMode        bool               `json:"debugMode" yaml:"debugMode"`
-	ExtensionSupport ExtensionSupport   `json:"extensionSupport" yaml:"extensionSupport"`
+	Heartbeat   HeartbeatConfig    `json:"heartbeat" yaml:"heartbeat"`
+	Logger      LoggerConfig       `json:"logger" yaml:"logger"`
+	Host        string             `json:"host" yaml:"host"`
+	Username    string             `json:"username" yaml:"username"`
+	Password    string             `json:"password" yaml:"password"`
+	Database    string             `json:"database" yaml:"database"`
+	Publication publication.Config `json:"publication" yaml:"publication"`
+	Slot        slot.Config        `json:"slot" yaml:"slot"`
+	Snapshot    SnapshotConfig     `json:"snapshot" yaml:"snapshot"`
+	Port        int                `json:"port" yaml:"port"`
+	Metric      MetricConfig       `json:"metric" yaml:"metric"`
+	DebugMode   bool               `json:"debugMode" yaml:"debugMode"`
 }
 
 type MetricConfig struct {
@@ -37,10 +36,6 @@ type MetricConfig struct {
 type LoggerConfig struct {
 	Logger   logger.Logger `json:"-" yaml:"-"`         // custom logger
 	LogLevel slog.Level    `json:"level" yaml:"level"` // if custom logger is nil, set the slog log level
-}
-
-type ExtensionSupport struct {
-	EnableTimeScaleDB bool `json:"enableTimeScaleDB" yaml:"EnableTimeScaleDB"`
 }
 
 type HeartbeatConfig struct {
